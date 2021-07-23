d72d98771ee17538938d45cead4bd61c48093019
HyeonseungSon <boywin8636@gmail.com>
2021-07-23 10:52:39 +0900

    ManDeum HyeonseungSon

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,7 @@
 /*
 Array
+ - 배열은 그 크기가 늘어나지 않습니다.
+
 1. Array()
 [형식]
 Array(개수, {식})
@@ -66,6 +68,58 @@ fun main() {
             println("array8.item : $item2")
         }
     }
+
+    println("----- 배열 출력 사용 방법 -----")
+
+    println("array1 0 : ${array1[0]}")
+    println("array1 1 : ${array1[1]}")
+    println("array1 2 : ${array1.get(2)}")
+    println("array1 3 : ${array1.get(3)}")
+
+    println("----- 배열 요소 변경 -----")
+
+    println("array1 : ${array1.contentToString()}")
+    array1[0] = 100
+    println("array1 : ${array1.contentToString()}")
+
+
+    println("----- 배열 크기 -----")
+
+    println("array1 size : ${array1.size}")
+
+    println("----- Kotlin 배열 제공 함수 -----")
+
+    println("array1 : ${array1.contentToString()}")
+
+    val array10 = array1.plus(60)
+    println("array1 : ${array1.contentToString()}") // 변화 없습니다.
+    println("array10 : ${array10.contentToString()}") // .plus 함수를 통해서 array10 이라는 새로운 배열을 생성해야 출력이 됩니다.
+
+    val array11 = array1.sliceArray(1..3) // 배열 요소 추출
+    println("array11 : ${array11.contentToString()}") // => array11 : [200, 30, 40]
+
+    println("첫 번째 값 : ${array1.first()}") // => 첫 번째 값 : 100
+    println("마지막 값 : ${array1.last()}") // => 마지막 값 : 50
+    println("30의 위치 : ${array1.indexOf(30)}") // => 30의 위치 : 2
+    println("평균 : ${array1.average()}") // => 평균 : 84.0
+    println("합 : ${array1.sum()}") // => 합 : 420
+    println("개수 : ${array1.count()}") // => 개수 : 5
+    println("개수 : ${array1.size}") // => 개수 : 5 (size Property)
+    println("30을 포함하는가 : ${array1.contains(30)}") // => 30을 포함하는가 : true
+    println("1000을 포함하는가 : ${array1.contains(1000)}") // => 1000을 포함하는가 : false
+    println("30을 포함하는가 : ${30 in array1}") // => 30을 포함하는가 : true
+    println("1000을 포함하는가 : ${1000 in array1}") // => 1000을 포함하는가 : false
+
+    val array15 = arrayOf(100, 200, 300)
+    // println("최대 : ${array15.max()}")
+    // println("최소 : ${array1.min()}")
+
+    val array12 = arrayOf(5, 1, 3, 7, 10, 8)
+    val array13 = array12.sortedArray()
+    val array14 = array12.sortedArrayDescending()
+    println("array13 : ${array13.contentToString()}") // => array13 : [1, 3, 5, 7, 8, 10]
+    println("array14 : ${array14.contentToString()}") // => array14 : [10, 8, 7, 5, 3, 1]
+
 }
 
 
